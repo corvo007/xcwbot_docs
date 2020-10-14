8550fa2f680d0163d41dd500fa5ab7df8c5fea3f
corvo007 <oscarlongsslz@gmail.com>
2020-10-14 20:15:14 +0800

    init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,106 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: '小仓唯Bot',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: '小仓唯Bot文档',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    logo:'https://image.pcrbotlink.top/favicon.ico',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    smoothScroll: true,
+    nextLinks: true,
+    // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
+    prevLinks: true,
+    nav: [
+      {
+        text: '主页',
+        link: 'https://pcrbotlink.top',
+      },
+      {
+        text: '常见问题',
+        link: '/guide/commonquestion'
+      },
+      {
+        text: '商店',
+        link: 'https://shop.pcrbotlink.top'
+      },
+      {
+        text: '购买指南',
+        link: '/shop/'
+      },
+      {
+        text: '促销活动',
+        link: '/shop/saleactivity'
+      },
+    ],
+    sidebar: [
+        {
+          title: '使用指南',
+          path: '/guide/',
+          collapsable: false,
+          children: [
+            '/guide/',
+            '/guide/updatelog',
+            '/guide/authsystem',
+            '/guide/clanbattle',
+            '/guide/practicalfunc',
+            '/guide/funfunc',
+            '/guide/commonquestion'
+          ]
+        },
+        {
+          title: '用户协议',
+          path: '/EULA/',
+          collapsable: false,
+          children: [
+            ['/EULA/','小仓唯Bot用户协议']
+          ]
+        },
+        {
+          title: '商店',
+          path: '/shop/',
+          collapsable: false,
+          children: [
+            '/shop/',
+            '/shop/saleactivity'
+          ]
+        }    
+    ]
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    'vuepress-plugin-clean-urls',
+    'vuepress-plugin-nprogress'
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://image.pcrbotlink.top/xcwtitle.jpg
+tagline: 多功能的公主连结Bot
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 高可用性 
+  details: 采用阿里云北京地域高配置服务器，并使用稳定性极佳的Bot后端，尽力减少计划外停机。
+- title: 高安全性
+  details: 所有站点均使用腾讯云CDN，提升访问速度的同时可以有效阻止恶意访客的破坏。
+- title: 高性价比
+  details: 每月最多只需花一杯多一点的奶茶钱，即可纵享仍在持续更新中的数十项功能。
+footer: 小仓唯Bot由Github/corvo007构建并维护
+---
